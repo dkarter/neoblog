06b9254f5868d07433bd87a1f8dd8da53e71c507
Dorian Karter <dkarter@users.noreply.github.com>
2025-03-21 12:56:45 -0500

    chore: personalize a bit

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 import { Github, Twitter, Linkedin, Mail } from "lucide-react";
 // Picture is being used as logo. Its grayscaled at homepage to match with the background glow.
 // Same can be created with different colors using AI tools. Also logo can be different ^^
-export const LOGO_URL = "/picture.webp";
+export const LOGO_URL = "/picture.jpg";
 export const SITE_LANGUAGE = "en-us";
-export const SITE_NAME = "Max Taun";
+export const SITE_NAME = "Dorian Karter";
 export const SITE_TITLE = "Welcome";
 export const SITE_DESCRIPTION =
   "Minimal dark portfolio theme, featuring easy customizations and well-commented code.";
@@ -11,17 +11,17 @@ export const SITE_IMAGE = "/astro-image.jpg";
 export const SOCIALS = [
   {
     name: "Github",
-    href: "https://github-url.com",
+    href: "https://github.com/dkarter",
     icon: Github,
   },
   {
     name: "Twitter",
-    href: "https://twitter-url.com",
+    href: "https://x.com/dorian_escplan",
     icon: Twitter,
   },
   {
     name: "LinkedIn",
-    href: "https://linkedin-url.com",
+    href: "https://www.linkedin.com/in/doriankarter/",
     icon: Linkedin,
   },
   {
